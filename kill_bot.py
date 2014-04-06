6916a0f0c48170c6ddeb393c718b48cba2cf6292
EC2 Default User <ec2-user@ip-172-31-41-25.us-west-2.compute.internal>
2014-04-06 18:47:07 +0000

    Make killbot actually work

--- kill_bot.py
+++ kill_bot.py
@@ -5,14 +5,10 @@ import signal
 
 #From https://github.com/getchar/rbb_article
 
-target = "HelpfulAppStoreBot.py"
+target = "HelpfulAppStore"
 
 # scan through processes
 for proc in psutil.process_iter():
-    # pay special attention to Python processes
-    if proc.name == "Python":
-        broken_path = proc.cmdline[-1].split("/")
-        source_f = broken_path[-1]
-        # if the source file name matches the target, kill the process
-        if source_f == target:
-            proc.send_signal(signal.SIGUSR1)
+    if proc.name() == target:
+        print(" match")
+        proc.send_signal(signal.SIGUSR1)
